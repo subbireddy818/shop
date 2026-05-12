13cccbe07eabde2560f2204f05f0b72340ae465a
subbireddy818 <your.sunnysubbireddy@gmail.com>
2026-05-12 12:25:25 +0530

    Fixing persistent Link import error in ProductDetail.tsx

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useParams, useNavigate, Link } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { allPlants, Plant } from '../data/plants'
 import { useCart } from '../context/CartContext'
 import { CategoriesSection } from '../components/CategoriesSection'
